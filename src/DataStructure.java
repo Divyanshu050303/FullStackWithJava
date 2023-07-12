99564b23f7c2850002ae3724244ed93516a53171
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-12 15:23:57 +0530

    FullStack With Java

---
+++ src/DataStructure.java
@@ -0,0 +1,21 @@
+public class DataStructure {
+    public static void main(String[] args) {
+
+//        Data Structure
+//        - Way of effienctly
+//        storing the data in memory;
+//        -Time and Space Complexity
+        /**
+         * Type of Ds
+         * 1 Primitive
+         * - Boolean
+         * -Char
+         * -int
+         * -Byte
+         * -Double
+         * -Float
+         *
+         *
+         * **/
+    }
+}
