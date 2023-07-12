ce3b98831bf8dfe13c6d9a63ca58ec52c94754fa
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-12 17:13:06 +0530

    Collection FameWork

--- src/DataStructure.java
+++ src/DataStructure.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 public class DataStructure {
     public static void main(String[] args) {
 
@@ -14,8 +16,47 @@ public class DataStructure {
          * -Byte
          * -Double
          * -Float
+         *You can store only single value
+         * 2: Non-Primitive
+         * type
+         * 1 linear
+         * -Array
+         * -String
+         * -List
+         * -Set
+         * -queue
+         * -ArrayList
+         * -Linked List
+         * -HashSet
+         * -Linked HashSet
+         * 2 non-linear
+         * -tree
+         * -graph
+         *  * Primitive you can create all type of convertor like temperature convertor
+         *  * Non-primitive you can create Banking Social Networking
+         *              Array                                               ArrayList
+         *      1 fixed size                                                    Not Fixed
+         *      2 Object value and primitive value                          Non-Primitive value only
+         *      3 fast as compare to arrayList                              Slow as compare to array
+         *      4 manual process/logic/algorithm fetching the value       PreDefined Methods
          *
+         *Type Safe:-if we define the datatype of arraylist then it did not accept any other value
          *
+         * Collection FrameWork :- Predefined Api used to store non-primitive data type
+         * Package :-
+         * Java.util.collection-arrayList, linkedList etc
+         * java.util.map- Key value pair forms data
+         * Iterable - Root
+         *                                      Collection
+         *                                           |
+         *                 |-------------------------------------------------------------|
+         *                 |                         |                                   |
+         *               List                        Set                                 Queue
+         *        --------|--------
+         *       |                |
+         *     ArrayList    Linked list         SortingSet HashSet Linked HashSet TreeSet
+
          * **/
+
     }
 }
