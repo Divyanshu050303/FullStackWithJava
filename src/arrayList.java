4ccce9d20acdae3eb00c1edeeb8eb4c506ac1887
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-14 21:56:04 +0530

    ArrayList

---
+++ src/arrayList.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+public class arrayList {
+    public static void main(String[] args) {
+        ArrayList arrayList=new ArrayList();
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+        arrayList.add(2,5);
+        System.out.println(arrayList);
+    }
+}
