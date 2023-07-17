e71a206fcada9cf957803a9bca3f32dcc7a6ac3d
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-17 15:35:27 +0530

    JDBC

---
+++ src/jdbc.java
@@ -0,0 +1,45 @@
+public class jdbc {
+    public static void main(String[] args) {
+        /*
+        * JDBC-Java database Connectivity
+        * ODBC- Open Database Connectivity
+        * Native Api
+        * Network Protocol
+        * Thin-Does not require native and middleware
+        *
+        * Steps for connectivity
+        * 1- Loading or registering the driver
+        * 2- Create the connection :-
+        * -->Tools 1. maven 2. gradle
+        * --> manual
+        *requirement:- URl:- DriverName+DBName, UserName: default root, Password: specific
+        * 3- Create Statement
+        * 4- Query
+        * 5- Execute
+        * 6- Result
+        * 7- Closing Connection
+        * Default PortNumber:- 3306
+        * Ide
+        * Strps for creating Project (Maven)
+        * 1- File-New-Project-Select Maven Architecture
+        * Give you project Name
+        * Install MySql
+        * -
+        *
+        * - opening the sql
+        * - execute the commands
+        * - create the project
+        * -
+        * -- Create DataBase "dataBase Name"
+        * -- Create Table Student(id integer, name char(50));
+        * -- insert into student( id, name) value(3, "Divyanshu");
+        * -- delete form student where id= 3
+        * -- update student set name="Isha" where id=3
+        * -- to show all the data select * from student
+        *
+        *
+        *
+        *
+        * */
+    }
+}
