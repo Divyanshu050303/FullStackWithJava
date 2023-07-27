d457d43d9dad5778b7668d15269f94c05a29b363
Divyanshu050303 <divyanshusingh7060@gmail.com>
2023-07-27 13:51:43 +0530

    Object Relational Mapping

---
+++ src/orm.java
@@ -0,0 +1,48 @@
+public class orm {
+    public static void main(String[] args) {
+        /*
+        * ORM -> Object Relational Mapping
+        * - Technique to mao the object in the database storage
+        * - Library - Simplifies the application development or saves time for creating application
+        * - Need to learn
+        * - Need to setup the environment
+        * Hibernate :- this is a java framework used to simplify the application  development
+        * - Data creation
+        * - Data manipulation
+        * - Data Access - Database Storage
+        * - internally uses the JDBC API
+        * - Javax.persistence - classes & interfaces
+        *  persistence the object store in the database
+        * - pojo class : Plain old java object
+        * - Dialects- used to interpret the code  in machine understandable code
+        * - How to use hibernate
+        * - steps
+        * 1- Create project - quick start
+        * 2- Add dependencies
+        *   -mysql
+        *   -hibernate
+        *   -configuration in properties(maven-source / maven-target) inside pom.xml
+        * 3-Creating connection  name.cfg.xml
+        *   - properties that we need to define
+        *       - connection driver
+        *       - hibernate connection
+        *       - username
+        *       - password
+        *       - dialect
+        *       - show sql query/result
+        *       - Hibernate to ddl
+        *       - Map the class
+        * 4- App.java
+        *   - Create the configuration object
+        *   - Read the configuration
+        *   - create the sessionFactory
+        *   - open session
+        *   - Start Transaction
+        *   - Create object of persistence class
+        *   - Save
+        *   - Commit
+        *   - Close the session
+        *   -
+        * */
+    }
+}
